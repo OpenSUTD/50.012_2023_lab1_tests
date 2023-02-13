17eb91a42e8657b1dfaef008ef642ac52d2b5999
Chester Koh <chester8991@gmail.com>
2023-02-13 20:17:38 +0800

    better error messages on monitor

--- test-client/tests/conftest.py
+++ test-client/tests/conftest.py
@@ -42,16 +42,42 @@ def check_proxy_alive(proxy_host, proxy_port):
     return check_proxy_alive_closure
 
 
+@pytest.fixture(scope="session")
+def zmq_socket(proxy_host):
+    context = zmq.Context()
+    _socket = context.socket(zmq.REQ)
+    _socket.connect(f"tcp://{proxy_host}:5555")
+    yield _socket
+    _socket.close()
+    context.term()
+
+
+def try_and_get_response_from_zmq_server(zmq_socket: zmq.Socket, timeout: float) -> str:
+    response = None
+    time_elapsed = 0
+    while response is None and time_elapsed < timeout:
+        try:
+            response = zmq_socket.recv(zmq.NOBLOCK).decode("utf-8")
+        except zmq.ZMQError:
+            time.sleep(0.2)
+            time_elapsed += 0.1
+            continue
+    return response
+
+
+premature_exit: bool = False
+
+
 @pytest.fixture()
-def restart_proxy(proxy_host, request):
+def restart_proxy(proxy_host, zmq_socket: zmq.Socket, request):
     def restart_proxy_closure():
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect(f"tcp://{proxy_host}:5555")
-        socket.send(f"begin_test {request.node.name}".encode("utf-8"))
-        response = socket.recv().decode("utf-8")
-        assert response == "restarted"
-        socket.close()
+        zmq_socket.send(f"begin_test {request.node.name}".encode("utf-8"))
+        response = try_and_get_response_from_zmq_server(zmq_socket, 15)
+        if response != "restarted":
+            global premature_exit
+            premature_exit = True
+            pytest.exit("The proxy runner was not able to restart your proxy."
+                        + " Check the logs of the proxy runner container and restart it if necessary.")
 
     return restart_proxy_closure
 
@@ -59,19 +85,21 @@ def restart_proxy(proxy_host, request):
 @pytest.fixture(scope="function", autouse=True)
 def setup_proxy_per_test(proxy_host, proxy_port, check_proxy_alive, restart_proxy):
     restart_proxy()
-    assert check_proxy_alive()
+    assert check_proxy_alive(), "The proxy does not seem to be running before the test starts"
     yield
-    assert check_proxy_alive()
+    assert check_proxy_alive(), "The proxy seems to have died after running the tests"
 
 
 @pytest.fixture(scope="session", autouse=True)
-def cleanup(proxy_host, request):
+def cleanup(proxy_host, zmq_socket: zmq.Socket, request):
     def send_end_tests_message():
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect(f"tcp://{proxy_host}:5555")
-        socket.send(f"end_tests".encode("utf-8"))
-        socket.close()
+        if premature_exit:
+            return
+        zmq_socket.send(f"end_tests".encode("utf-8"))
+        response = try_and_get_response_from_zmq_server(zmq_socket, 15)
+        if response != "ended":
+            pytest.exit("The proxy runner was not able to restart your proxy at the end of the test suite."
+                        + " Check the logs of the proxy runner container and restart it if necessary.")
 
     request.addfinalizer(send_end_tests_message)
 
